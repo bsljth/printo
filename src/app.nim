d0d626f903e3c3aa79a52f498481ef16ada3eeb8
Basil Ajith <bsljth@gmail.com>
2023-07-06 13:54:37 +0530

    removed test app

--- src/app.nim
+++
@@ -1,5 +0,0 @@
-from printo import printo
-
-let txt = "Lorem ipsum whatever you've got in this marvellous torrent of a story. And such is the need of the hour for an assessment to be done in the light of the facts and circumstances."
-
-printo(txt, "wbwnl", 50)
