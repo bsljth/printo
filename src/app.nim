d3891b30f88fc5a787a267635be15e9744a83497
Basil Ajith <bsljth@gmail.com>
2023-07-06 13:53:49 +0530

    finished v0.1.0...yeah!

---
+++ src/app.nim
@@ -0,0 +1,5 @@
+from printo import printo
+
+let txt = "Lorem ipsum whatever you've got in this marvellous torrent of a story. And such is the need of the hour for an assessment to be done in the light of the facts and circumstances."
+
+printo(txt, "wbwnl", 50)
--- src/printo.nim
+++ src/printo.nim
@@ -1,14 +1,8 @@
 import std/strutils
+import std/strformat
 from os import sleep
 
-
-#[ proc wbwsl(text: string, timeMS: int) =
-  var arr = split(text)
-
-  for c in text:
-    sleep(timeMS)    
-    echo c ]#
-
+## print text letter by letter on the same line
 proc lblsl(text: string, timeMS: int) =
   setStdIoUnbuffered()
   for c in text:
@@ -16,17 +10,39 @@ proc lblsl(text: string, timeMS: int) =
     stdout.write(c)
   stdout.write("\n")
 
+## print text word by word on the same line
+proc wbwsl(text: string, timeMS: int) =
+  var arr = split(text)
+
+  setStdIoUnbuffered()
+  for ix, elmnt in arr:
+    if ix == elmnt.len - 1:
+      stdout.write(elmnt)
+    else:
+      sleep(timeMS)    
+      stdout.write(elmnt & " ")
+  stdout.write("\n")
+
+proc lblnl(text: string, timeMS: int) =
+  for c in text:
+    sleep(timeMS)
+    echo(c)
+
+
+proc wbwnl(text: string, timeMS: int) =
+  var arr = split(text)
+  for item in arr:
+    sleep(timeMS)
+    echo(item)
+
 proc printo*(text: string, mode: string, timeMS: int) =
-  #[ if mode == "wbwsl":
-    wbwsl(text, timeMS) ]#
   if mode == "lblsl":
     lblsl(text, timeMS)
-  #[ elif mode == "wbwnl":
-    wbwnl(text, timeMS)
+  elif mode == "wbwsl":
+    wbwsl(text, timeMS)
   elif mode == "lblnl":
-    lblnl(text, timeMS) ]#
-
-
-#proc lblsl(text: string, timeMS: uint) =
-#proc wbwnl(text: string, timeMS: uint) =
-#proc lblnl(text: string, timeMS: uint) =
+    lblnl(text, timeMS)
+  elif mode == "wbwnl":
+    wbwnl(text, timeMS)
+  else:
+    raise newException(ValueError, fmt"Mode {mode} is not found! Must be: lblsl or wbwsl or lblnl or wbwnl")
